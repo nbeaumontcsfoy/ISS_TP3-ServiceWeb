ee81651384c28f0696cf41622671d0041b3e72ab
nbeaumontcsfoy <nicolasbeaumont01@gmail.com>
2026-05-27 11:11:24 -0400

    création structure dossiers

---
+++ serveur1/html/index.php
@@ -0,0 +1,15 @@
+<h1>Je te test!</h1>
+<h4>Tentative de connexion Mariadb depuis PHP...</h4>
+<?php
+	//phpinfo();
+$host = 'mariadb';
+$user = $_ENV["DB_USER"];
+$pass = $_ENV["DB_PASSWD"];
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+	die("La connexion a échoué: " . $conn->connect_error);
+}
+echo "Connexion réussie à MariaDB!";
+?>
+
