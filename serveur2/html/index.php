2f32b138560f223cb0e49079b0cb72c158d41334
nbeaumontcsfoy <nicolasbeaumont01@gmail.com>
2026-05-30 14:36:52 -0400

    modifs default.conf, Dockerfile, httpd.conf, index.html .php

--- serveur2/html/index.php
+++ serveur2/html/index.php
@@ -1,8 +1,8 @@
 <h1>Je te test!</h1>
-<h4>Tentative de connexion Mariadb depuis PHP...</h4>
+<h4>Tentative de connexion MySQL depuis PHP...</h4>
 <?php
 	//phpinfo();
-$host = 'mariadb';
+$host = 'mysql';
 $user = $_ENV["DB_USER"];
 $pass = $_ENV["DB_PASSWD"];
 $conn = new mysqli($host, $user, $pass);
@@ -10,6 +10,6 @@ $conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
 	die("La connexion a échoué: " . $conn->connect_error);
 }
-echo "Connexion réussie à MariaDB!";
+echo "Connexion réussie à MySQL!";
 ?>
 
